262b25b5ba072ab18cdb4279c57b6b37bc929bb5
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2026-04-06 12:06:19 -0300

    fix - migration orders

--- database/migrations/2026_04_04_185558_create_orders_table.php
+++ database/migrations/2026_04_04_185558_create_orders_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
-            $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->enum('payment_status', ['pending', 'paid', 'canceled'])->default('pending');
             $table->string('payment_url')->nullable();
             $table->string('payment_token')->nullable();
